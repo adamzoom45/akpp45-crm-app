3f16efb7eff3bd4d6a931abd1bea152c6c78e531
adamzoom45 <adamzoom45@gmail.com>
2026-06-14 19:51:01 +0500

    MainActivity.java

--- app/src/main/java/com/akpp45/crm/MainActivity.java
+++ app/src/main/java/com/akpp45/crm/MainActivity.java
@@ -5,21 +5,17 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-    private SwipeRefreshLayout swipeRefresh;
     private ProgressBar progressBar;
-    private boolean isLoading = true;
 
     private static final String CRM_URL = "https://akpp45.ru/wp-admin/admin.php?page=akpp-crm";
     private static final String DOMAIN = "akpp45.ru";
@@ -31,19 +27,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
-        swipeRefresh = findViewById(R.id.swipeRefresh);
         progressBar = findViewById(R.id.progressBar);
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setCacheMode(android.webkit.WebSettings.LOAD_DEFAULT);
+        webView.getSettings().setCacheMode(2);
 
         webView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                String url = request.getUrl().toString();
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 if (url.contains(DOMAIN)) {
                     return false;
                 }
@@ -54,9 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPageFinished(WebView view, String url) {
-                progressBar.setVisibility(android.view.View.GONE);
-                swipeRefresh.setRefreshing(false);
-                isLoading = false;
+                progressBar.setVisibility(View.GONE);
             }
         });
 
@@ -64,14 +56,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
                 progressBar.setProgress(newProgress);
-            }
-        });
-
-        swipeRefresh.setOnRefreshListener(() -> {
-            if (!isLoading) {
-                webView.reload();
-            } else {
-                swipeRefresh.setRefreshing(false);
             }
         });
 
@@ -85,17 +69,5 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return super.onKeyDown(keyCode, event);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        webView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        webView.onPause();
     }
 }
